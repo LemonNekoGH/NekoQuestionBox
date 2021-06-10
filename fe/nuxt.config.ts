db7a460eb8a6e983615074d870c380c66507afc6
LemonNeko <chheese048@gmail.com>
2021-06-10 12:27:19 +0800

    :wheelchair: 增加推特小卡片

--- fe/nuxt.config.ts
+++ fe/nuxt.config.ts
@@ -18,7 +18,12 @@ const config: NuxtConfig = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'LemonNeko\'s Question Box' }
+      { hid: 'description', name: 'description', content: 'LemonNeko\'s Question Box' },
+      { name: 'twitter:card', content: 'summary' },
+      { name: 'twitter:creator', content: '@lemon_neko_cn' },
+      { property: 'og:image', content: 'https://qbox.lemonneko.moe/question_48x48.png' },
+      { property: 'og:title', content: '柠喵的问题箱' },
+      { property: 'og:description', content: '在这里，大家可以匿名向柠喵提问哦' }
     ],
     link: [
       { rel: 'icon', type: 'image/png', href: '/favicon.png' }
@@ -33,10 +38,11 @@ const config: NuxtConfig = {
     '@nuxtjs/vuetify'
   ],
   modules: [
-    '@nuxtjs/axios'
+    '@nuxtjs/axios',
+    '@nuxtjs/robots'
   ],
-  axios: {
-
+  robots: {
+    UserAgent: '*'
   },
   vuetify: {
     theme: {
