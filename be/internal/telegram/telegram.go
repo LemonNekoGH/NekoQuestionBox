5c73f37199c7cc56fdb90d97fe2f4b543ac19fc6
LemonNeko <chheese048@gmail.com>
2022-09-24 16:01:55 +0800

    :construction: 添加初始化 TG Bot 函数

---
+++ be/internal/telegram/telegram.go
@@ -0,0 +1,5 @@
+package telegram
+
+func InitTG() {
+
+}
--- be/main/main.go
+++ be/main/main.go
@@ -5,12 +5,14 @@ import (
 	"neko-question-box-be/internal/database"
 	"neko-question-box-be/internal/logger"
 	"neko-question-box-be/internal/server"
+	"neko-question-box-be/internal/telegram"
 )
 
 func main() {
 	logger.InitLogger()
 	config.InitConfig(false)
 	database.InitDB()
+	telegram.InitTG()
 
 	r := server.InitServer()
 	r.Run()
